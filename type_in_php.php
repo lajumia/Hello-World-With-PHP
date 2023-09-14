336a9d610818c042ea1bb2a44dc78c903048a26e
Md Laju Mia <developerlaju@gmail.com>
2023-09-14 06:13:38 -0700

    PHP type explain

---
+++ type_in_php.php
@@ -0,0 +1,23 @@
+<?php 
+// List of type in PHP
+
+$null = null;//Type is null
+$string = "PHP";//Type is String
+$int = 234; // Type is Integer
+$float = 23.4; //Type is Float
+$bool = true; //Type is Boolean
+$array = ['a','b','[c']; // Type is Array
+$obj = new className(); //Type is Object
+$resource = fopen('some.txt', "w"); //Type is resource
+
+
+
+
+Class className{
+ //Do somthing here
+}
+
+
+
+
+?>
